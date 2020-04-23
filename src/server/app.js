94d895d04482631af1fbcecbac914bc6d3bc44dd
DBuckley0126 <DannyBoy.0126>
2020-04-23 23:45:26 +0100

    Create initial app

---
+++ src/server/app.js
@@ -0,0 +1,30 @@
+const Koa = require("koa");
+const mount = require('koa-mount');
+const logger = require("koa-logger");
+const serve = require('koa-static');
+const KoaBody = require("koa-body");
+const Json = require("koa-json");
+const cors = require("@koa/cors");
+
+const indexRoutes = require("./routes/index");
+
+const app = new Koa();
+const PORT = process.env.PORT || 5000;
+
+
+// JSON Prettier middleware
+app.use(Json());
+// Add koa-body middleware
+app.use(KoaBody());
+// Add koa-logger middleware
+app.use(logger());
+// Add koa-cors middleware
+app.use(cors());
+
+app.use(indexRoutes.routes(app));
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
