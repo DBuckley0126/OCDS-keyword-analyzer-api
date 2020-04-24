b8fcdaa202d14fa29ebdd0affafd3f6cc6289dd8
DBuckley0126 <DannyBoy.0126>
2020-04-24 01:25:31 +0100

    Add keyword analysis manager with tests

---
+++ src/managers/ldaManager.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-restricted-syntax */
+const lda = require("lda");
+const CheckWord = require("check-word");
+const pluralize = require("pluralize");
+const dictionary = require("../lib/dictionary");
+// const errorManager = require("./errorManager");
+
+const words = CheckWord("en");
+
+const wordChecker = word => {
+  switch (true) {
+    case dictionary.blacklistWords.includes(word):
+      return false;
+    case dictionary.whitelistWords.includes(word):
+      return true;
+    case words.check(word):
+      return true;
+    default:
+      return false;
+  }
+};
+
+const ldaManager = {
+  getTopicWords: (documentArray, amountOfTopics = 1, keywordsPerTopic = 2) => {
+    const checkedDocumentArray = documentArray.map(string => {
+      return string.replace(/\b&#.{0,3};\b/g, "");
+    });
+    let outputArray = [];
+    try {
+      let attempts = 0;
+      while (
+        outputArray.length < amountOfTopics * keywordsPerTopic &&
+        attempts < 4
+      ) {
+        outputArray = [];
+        const results = lda(
+          checkedDocumentArray,
+          amountOfTopics,
+          keywordsPerTopic + attempts
+        );
+
+        for (topic of results) {
+          for (keyword of topic) {
+            const singularVersion = pluralize.singular(keyword.term);
+            if (
+              wordChecker(singularVersion) &&
+              outputArray.length < amountOfTopics * keywordsPerTopic
+            ) {
+              outputArray.push(pluralize.singular(keyword.term));
+            } else if (outputArray.length < amountOfTopics * keywordsPerTopic) {
+              console.log(`Rejected Keyword: ${singularVersion}`);
+            }
+          }
+        }
+        attempts += 1;
+      }
+    } catch (err) {
+      err.code = "LDA_FAIL";
+      errorManager.handleError(err);
+    } finally {
+      return outputArray;
+    }
+  }
+};
+
+module.exports = ldaManager;
